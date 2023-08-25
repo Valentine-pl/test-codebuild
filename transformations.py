423bb26c602f02a0ab38cdba32b16a0139e70b9b
Valentine-pl <valentinpl737@gmail.com>
2023-08-25 21:13:49 +0300

    update transformations

--- transformations.py
+++ transformations.py
@@ -3,4 +3,4 @@
 from pyspark.sql.functions import lit
 
 def transformation_func(df):
-    return df.withColumn("new_column", lit("new_value_super"))
+    return df.withColumn("new_column", lit("new_value_good"))

0fd51df7f394fc2c5df14c9d8972aa479277d6b7
Valentine-pl <valentinpl737@gmail.com>
2023-08-25 22:33:48 +0300

    remove column

--- transformations.py
+++ transformations.py
@@ -3,6 +3,8 @@
 from pyspark.sql.functions import lit
 
 def transformation_func(df):
-    df = df.withColumn("new_column1", lit("new_value1"))
+    #df = df.withColumn("new_column1", lit("new_value1"))
     df = df.withColumn("new_column2", lit("new_value2"))
+   # Drop the column 'new_column1'
+    df = df.drop("new_column1")
     return df

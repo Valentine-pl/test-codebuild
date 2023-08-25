e49ae26acf26c48800c9a94a8984048002c1c056
Valentine-pl <valentinpl737@gmail.com>
2023-08-25 22:23:48 +0300

    add second new column

--- transformations.py
+++ transformations.py
@@ -4,4 +4,5 @@ from pyspark.sql.functions import lit
 
 def transformation_func(df):
     df = df.withColumn("new_column1", lit("new_value1"))
+    df = df.withColumn("new_column2", lit("new_value2"))
     return df
